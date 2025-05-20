0f11a14bc739fb20625c13ce01a71db3797c4415
Fred Wen <fredwen2020@outlook.com>
2025-05-20 16:04:25 +0800

    working

--- resources/txt2words.py
+++ resources/txt2words.py
@@ -73,8 +73,7 @@ def split_pinyin(pinyin):
     # 可选的声母部分+韵母部分
     syllable_pattern = rf"({'|'.join(initials)})?({'|'.join(tone_finals)})"
     for part in parts:
-        # 处理撇号分隔的部分（例如 xi'an → xi + an）
-        sub_parts = part.split('\'')
+        sub_parts = part.split(' ')
         for sub_part in sub_parts:
             if not sub_part:
                 continue
@@ -103,7 +102,8 @@ def parse_cidian():
     # 词条格式为：【词】拼音 释义
     pattern = r'【(.*?)】\s*\d*\s*([a-zA-Zāáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜ̀ ]+)\s*(.*)'
     entries = re.findall(pattern, content)
-    
+    user_words = set()
+
     # 存储词条信息
     dictionary = {}
     for word, pinyin,defs in entries:
@@ -114,6 +114,12 @@ def parse_cidian():
             if pinyin_list[-1] == "r" and  not word.endswith("儿"):
                 pinyin_list = pinyin_list[:-1]
             dictionary[word] = {'pinyin': pinyin_list, 'explanation': defs}
+            user_words.add(word+ " 10000")
+    # 生成分词词典
+    if not os.path.exists('user_words.txt'):
+        with open('user_words.txt', 'w', encoding='utf-8') as f:
+            for word in user_words:
+                f.write(word + "\n")
     
     return dictionary
 
@@ -135,7 +141,13 @@ def parse_keben():
 
     # 用于存储所有不重复的词语
     all_words = set()
-
+    all_chars = set()
+    # load user words to jieba
+    if os.path.exists('user_words.txt'):
+        with open('user_words.txt', 'r', encoding='utf-8') as f:
+            user_words = set(f.read().splitlines())
+        jieba.load_userdict(user_words)
+    jieba.del_word("匍匐前进")
     # 遍历每个txt文件
     for txt_file in txt_files:
         print(f'Processing {txt_file}...')
@@ -143,17 +155,23 @@ def parse_keben():
         # 读取文件内容
         with open(txt_file, 'r', encoding='utf-8') as f:
             text = f.read()
-        
         # 只保留汉字和标点符号
-        text = re.sub(r'[^\u4e00-\u9fff\s]', '', text)
-        
+        text = re.sub(r'[^\u4e00-\u9fff]', '', text)
+        text = text.replace('\n', '')
+        chars = set(text)
+        all_chars.update(chars)
         # 使用jieba进行分词
         words = jieba.cut(text)
+        words = list(words)
+        # write to a file name .jieba
+        with open(txt_file + '.jieba', 'w', encoding='utf-8') as f:
+            f.write(' '.join(words))
         
         # 将词语添加到集合中
         all_words.update(words)
-    return all_words
+    return all_words,all_chars
 
+# 读取常用成语
 def parse_chengyu300():
     with open('成语300.txt', 'r', encoding='utf-8') as f:
         content = f.read()
@@ -164,8 +182,6 @@ def parse_chengyu300():
                 words.add(word)
     return words
 
-
-
 if __name__ == '__main__':
     # 解析现代汉语词典
     cidian = parse_cidian()
@@ -173,16 +189,50 @@ if __name__ == '__main__':
     # 解析成语词典
     chengyu = parse_chengyu()
 
-    # 解析课本
-    words = parse_keben()
-
+    # 解析课本, 得到课本中所有的词语和字符
+    words,chars = parse_keben()
+    
     # 解析成语3000
     chengyu300 = parse_chengyu300()
 
     all_words = words | chengyu300
 
+    out_words = {}
     for word in sorted(all_words):
-        if word in cidian:
-            print(word," ", cidian[word]['pinyin'], " ", cidian[word]['explanation'])
-        elif word in chengyu:
+        if word in chengyu:
+            out_words[word] = {'pinyin': chengyu[word]['pinyin'], 'explanation': chengyu[word]['explanation'], "chengyu": True}
             print(word," ", chengyu[word]['pinyin'], " ", chengyu[word]['explanation'])
+        elif word in cidian:
+            out_words[word] = {'pinyin': cidian[word]['pinyin'], 'explanation': cidian[word]['explanation'], "chengyu": False}
+            print(word," ", cidian[word]['pinyin'], " ", cidian[word]['explanation'])
+        elif len(word)>2:
+            words1 = jieba.lcut(word)
+            for word1 in words1:
+                if word1 in cidian:
+                    out_words[word] = {'pinyin': cidian[word1]['pinyin'], 'explanation': cidian[word1]['explanation'], "chengyu": False}
+                    print(word," ", cidian[word1]['pinyin'], " ", cidian[word1]['explanation'])
+                    break
+    for char in sorted(chars):
+        has_word = False
+        for word in out_words:
+            if char in word:
+                has_word = True
+                break
+                # print(char," ------ ", word)
+        if not has_word:
+            for word in cidian:
+                if word.startswith(char):
+                    out_words[word] = {'pinyin': cidian[word]['pinyin'], 'explanation': cidian[word]['explanation'], "chengyu": False}
+                    print(word," ", cidian[word]['pinyin'], " ", cidian[word]['explanation'])
+                    has_word = True
+                    break
+        if not has_word:
+            for word in cidian:
+                if char in word:
+                    out_words[word] = {'pinyin': cidian[word]['pinyin'], 'explanation': cidian[word]['explanation'], "chengyu": False}
+                    print(word," ", cidian[word]['pinyin'], " ", cidian[word]['explanation'])
+                    has_word = True
+                    break
+    # 将out_words写入all_words.json
+    with open('all_words.json', 'w', encoding='utf-8') as f:
+        json.dump(out_words, f, ensure_ascii=False)
